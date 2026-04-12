ed66d9ba6111998bfd5b5fa817d353e44ccd6d89
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-04-12 00:59:06 -0300

    Add files via upload

---
+++ test/src/pages/dashboard.tsx
@@ -0,0 +1,33 @@
+import './styles/dashboard.css'
+
+function Dashboard(){
+
+	return(
+
+		<div className="body">
+			<aside className="aside">
+				<section className="option_button">
+				
+  				</section>
+
+
+				<section className="option_button">
+
+  				</section>
+	
+  			</aside>
+
+
+			<section className="header">
+
+  			</section>
+
+
+			<section className="main">
+
+  			</section>
+		</div>
+
+    			
+);
+}
