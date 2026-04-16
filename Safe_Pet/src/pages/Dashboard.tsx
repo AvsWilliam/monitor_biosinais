0c1847b95c35613e474535b2430d8f5b6c9f0b78
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-04-16 16:02:22 -0300

    Update Dashboard.tsx

--- Safe_Pet/src/pages/Dashboard.tsx
+++ Safe_Pet/src/pages/Dashboard.tsx
@@ -1,18 +1,53 @@
 import React from 'react';
 import '../styles/Dashboard.css';
+import {useNavigate} from 'react-router-dom'
+
 
 export default function Dashboard(){
+
+	const navigate = useNavigate();
+				
+	function go_to_dashboard(){
+     		navigate('/dashboard');
+	}
+
+	function go_to_mapas(){
+     		navigate('/mapas');
+	}
+
+	function go_to_saude(){
+     		navigate('/saude');
+	}
+
+	function go_to_conectividade(){
+     		navigate('/conectividade');
+	}
+	function go_to_configuracoes(){
+     		navigate('/configuracoes');
+	}
 
 	return(
 
 		<div className="corpo">
 			<aside className="aside">
-				<section className="option_button">
-					<p>aside button</p>
+				<section className="option_button" onClick = {go_to_dashboard}>
+					<p>Dashboard</p>
   				</section>
 
-				<section className="option_button">
-					<p>aside button</p>
+				<section className="option_button" onClick = {go_to_mapas}>
+					<p>Mapas</p>
+  				</section>
+
+				<section className="option_button" onClick = {go_to_saude}>
+					<p>Saúde</p>
+  				</section>
+
+				<section className="option_button" onClick = {go_to_conectividade}>
+					<p>Conectividade</p>
+  				</section>
+
+				<section className="option_button" onClick = {go_to_configuracoes}>
+					<p>Configurações</p>
   				</section>
 	
   			</aside>
