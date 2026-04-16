ebec2a0106ebccae105dbef88f90e832ad61099d
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-04-16 16:04:22 -0300

    Update Routes.tsx

--- Safe_Pet/src/Routes.tsx
+++ Safe_Pet/src/Routes.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Dashboard from './pages/Dashboard.tsx';
+import Mapas from './pages/mapas.tsx';
+import Saude from './pages/saude.tsx';
+import Conectividade from './pages/conectividade.tsx';
+import Configuracoes from './pages/configuracoes.tsx';
 import './styles/Dashboard.css';
 
 export default function Routing(){
@@ -10,6 +14,11 @@ export default function Routing(){
 		<BrowserRouter>
     			<Routes>
 				<Route path="/" element={<Dashboard />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/mapas" element={<Mapas />} />
+				<Route path="/saude" element={<Saude />} />
+				<Route path="/conectividade" element={<Conectividade />} />
+				<Route path="/configuracoes" element={<Configuracoes />} />
 			</Routes>
 		</BrowserRouter>
 		
