46a60e5cd50e19887509203edf05137c4afb2ee8
ViniciusFS1 <fioravante.vinicius@gmail.com>
2026-04-27 18:17:56 -0300

    Add files via upload

---
+++ Safe_Pet/src/components/item.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Dashboard.css';
+
+
+export default function Item(){
+
+	
+	return(
+
+		<div className="item">
+
+			<p>conteúdo aqui</p>
+
+		</div>
+
+    			
+);
+}
